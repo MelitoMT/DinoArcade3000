07ff498d501b5751cfc8d222287dc02850800a4e
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-06-17 19:10:59 -0500

    FEAT:Inicio responsivo

---
+++ Dynamics/JS/Interior.js
@@ -0,0 +1,40 @@
+{/* <header class="nav">
+<nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Puntajes</a></li>
+    <li><a href="#">DinoJuegos</a></li>
+    <li><a href="#">Ayuda</a></li>
+    <li><a href="#">Créditos</a></li>        
+  </ul>
+</nav>
+</header>  
+<div class="flex">
+<div class="wall">
+    <div class="wLogo"></div>
+    <div class="wScore">
+        <span class="score"><a href="#" class="none">PUNTAJES</a></span>
+    </div>
+    <div class="wHelp">
+        <span class="help"><a href="#" class="none">AYUDA</a></span>
+    </div>
+</div>
+<a href="#" class="none"><div class="arcade">
+    <div class="screen">
+        <span id="dino">DINO</span>
+    </div>
+</div></a>
+</div>
+
+ */}
+let body = document.getElementsByTagName("body");
+let nav = document.querySelector("nav");
+let flex = document.querySelector(".flex");
+let nav = document.querySelector(".nav");
+let wall = document.querySelector(".wall");
+let wLogo = document.querySelector(".wLogo");
+let wScore = document.querySelector(".wScore");
+let wHelp = document.querySelector(".wHelp");
+let none = document.querySelector(".none");
+let screen = document.querySelector(".arcade");
+let boton = document.querySelector("#boton");
