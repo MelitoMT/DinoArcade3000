ee53a54315d6b7b139d9054be7da87bc1bd01dae
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-06-17 23:39:25 -0500

    FEAT:Toggle claro y oscuro interior

--- Dynamics/JS/Interior.js
+++ Dynamics/JS/Interior.js
@@ -1,40 +1,23 @@
-{/* <header class="nav">
-<nav>
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Puntajes</a></li>
-    <li><a href="#">DinoJuegos</a></li>
-    <li><a href="#">Ayuda</a></li>
-    <li><a href="#">Créditos</a></li>        
-  </ul>
-</nav>
-</header>  
-<div class="flex">
-<div class="wall">
-    <div class="wLogo"></div>
-    <div class="wScore">
-        <span class="score"><a href="#" class="none">PUNTAJES</a></span>
-    </div>
-    <div class="wHelp">
-        <span class="help"><a href="#" class="none">AYUDA</a></span>
-    </div>
-</div>
-<a href="#" class="none"><div class="arcade">
-    <div class="screen">
-        <span id="dino">DINO</span>
-    </div>
-</div></a>
-</div>
-
- */}
 let body = document.getElementsByTagName("body");
-let nav = document.querySelector("nav");
-let flex = document.querySelector(".flex");
-let nav = document.querySelector(".nav");
-let wall = document.querySelector(".wall");
-let wLogo = document.querySelector(".wLogo");
-let wScore = document.querySelector(".wScore");
-let wHelp = document.querySelector(".wHelp");
-let none = document.querySelector(".none");
-let screen = document.querySelector(".arcade");
-let boton = document.querySelector("#boton");
+let blue = document.querySelectorAll(".blue");
+let bg = document.querySelector(".flexC");
+let shadow = document.querySelectorAll(".LSHC");
+let arcade = document.querySelector(".arcadeC");
+let boton = document.querySelector(".slider");
+boton.addEventListener("click",colors);
+function colors(){
+    console.log("color");
+    blue[0].classList.toggle("pink");
+    blue[1].classList.toggle("pink");
+    blue[2].classList.toggle("pink");
+    blue[3].classList.toggle("pink");
+    blue[4].classList.toggle("pink");
+    blue[5].classList.toggle("pink");
+    blue[6].classList.toggle("pink");
+    bg.classList.toggle("flexC2");
+    shadow[0].classList.toggle("LSHC2");
+    shadow[1].classList.toggle("LSHC2");
+    shadow[2].classList.toggle("LSHC2");
+    arcade.classList.toggle("arcadeC2");
+    window.location.reload;
+}
