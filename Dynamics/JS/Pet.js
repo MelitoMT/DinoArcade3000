b9c5bed76d9405ee5bc60655da44e9266a40fa03
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-06-19 01:12:50 -0500

    Cambios en créditos

--- Dynamics/JS/Pet.js
+++ Dynamics/JS/Pet.js
@@ -11,14 +11,36 @@ function contents(file){
         console.log(error.message)
     })
 }
-function games(file){
+function interior(file){
     fetch(file)
     .then((respuesta)=>{
         return respuesta.text();
     })
     .then((texto)=>{
-        let general = document.querySelector("#general");
-        general.innerHTML = texto;
+        let content = document.querySelector("#content");
+        content.innerHTML = texto;
+        let blue = document.querySelectorAll(".blue");
+        let bg = document.querySelector(".flexC");
+        let shadow = document.querySelectorAll(".LSHC");
+        let arcade = document.querySelector(".arcadeC");
+        let boton = document.querySelector(".slider");
+        boton.addEventListener("click",colors);
+        function colors(){
+            console.log("color");
+            blue[0].classList.toggle("pink");
+            blue[1].classList.toggle("pink");
+            blue[2].classList.toggle("pink");
+            blue[3].classList.toggle("pink");
+            blue[4].classList.toggle("pink");
+            blue[5].classList.toggle("pink");
+            blue[6].classList.toggle("pink");
+            bg.classList.toggle("flexC2");
+            shadow[0].classList.toggle("LSHC2");
+            shadow[1].classList.toggle("LSHC2");
+            shadow[2].classList.toggle("LSHC2");
+            arcade.classList.toggle("arcadeC2");
+            window.location.reload;
+        }
     })
     .catch((error)=>{
         console.log(error.message)
@@ -30,12 +52,12 @@ fetch('Nav.html')
         return response.text();
     })
     .then((text)=>{
-        console.log(text);
         let nav = document.querySelector("#nav");
         nav.innerHTML = text;
-        contents('Interior.html')
+        interior('Interior.html')
         document.querySelector("#Inicio").addEventListener("click",()=>{
-            contents('Interior.html');
+            interior('Interior.html');
+            
         })
         document.querySelector("#DinoJuegos").addEventListener("click",()=>{
             fetch('Menu.html')
@@ -45,16 +67,7 @@ fetch('Nav.html')
             .then((text)=>{
                 let content = document.querySelector("#content");
                 content.innerHTML = text;
-/*                 document.querySelector("#Juego1").addEventListener("click",()=>{
-                    games('juego1.html');
-                })
-                document.querySelector("#Juego2").addEventListener("click",()=>{
-                    games('Juego2.html');
-                })
-                document.querySelector("#Juego3").addEventListener("click",()=>{
-                    games('Juego3.html');
-                })
- */            })
+            })
             .catch((error)=>{
                 console.log(error.message)
             })
